100a381ce0cd288dec87d6b2b718fac403269555
NajlaBH-Angular <BHNajla.bioinfo@gmail.com>
2021-11-25 08:14:24 +0000

    :wrench: Add Local image.

--- src/app/cbase/cbase.component.ts
+++ src/app/cbase/cbase.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CbaseComponent implements OnInit {
 
-  imagePath:string="https://t4.ftcdn.net/jpg/01/27/67/79/360_F_127677934_R80d0Zy2MnHfmflBF8agHKMHZWhiDMMU.jpg"
+  //imagePath:string="https://t4.ftcdn.net/jpg/01/27/67/79/360_F_127677934_R80d0Zy2MnHfmflBF8agHKMHZWhiDMMU.jpg"
+  imagePath:string="assets/img/agenda.png"
   constructor() { }
 
   ngOnInit(): void {
