a931ac05e855618f66d0050047aaac5c5fc32f6d
NajlaBH-Angular <BHNajla.bioinfo@gmail.com>
2021-11-30 14:44:35 +0000

    :bug: Fix Delete func.

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -8,16 +8,23 @@ import { Contact } from '../models/contact';
 export class ContactService {
   //Initialize contact table
   contactserv:Contact[] = [
-    new Contact(0,'test user','testuser@gmail.com','+21600000000','1020 Tunis.'),
-    new Contact(1,'dev user','devuser@gmail.com','+00336908890','75 Paris.'),
-    new Contact(2,'help desk', 'helpdesk@gmail.com','+00434098678','Swiss.')
+    new Contact(1,'test user','testuser@gmail.com','+21600000000','1020 Tunis.'),
+    new Contact(2,'dev user','devuser@gmail.com','+00336908890','75 Paris.'),
+    new Contact(3,'help desk', 'helpdesk@gmail.com','+00434098678','Swiss.')
   ];
 
 
   constructor() { }
 
   //Delete task
-  deleteContact(indx:number) {
-        this.contactserv.splice(indx, 1);
+  deleteContact(key: number) {
+    this.contactserv.forEach((value,index)=>{
+        if(value.contactid==key) 
+	   this.contactserv.splice(index,1);
+           console.log("index deleted:", index);
+
+        });
+    //this.contactserv.splice(indx, 1);
+    //console.log("index deleted:", indx);
   }
 }
